c3c46b00ebff4c16de6c4504899025c12b4e134d
Nasim <mdnasimhosen333@gmail.com>
2026-01-15 15:23:30 +0600

    starting project

---
+++ src/app/modules/User/user.interface.ts
@@ -0,0 +1,27 @@
+/* eslint-disable no-unused-vars */
+import { Model } from "mongoose";
+import { USER_ROLE } from "./user.const";
+
+export interface TUser {
+  name: string;
+  email: string;
+  password: string;
+  role: "superAdmin" | "user";
+}
+
+export type TLoginUser = {
+  email: string;
+  password: string;
+};
+
+export interface UserModel extends Model<TUser> {
+  //instance methods for checking if the user exist
+  isUserExistsByEmail(email: string): Promise<TUser>;
+  //instance methods for checking if passwords are matched
+  isPasswordMatched(
+    plainTextPassword: string,
+    hashedPassword: string
+  ): Promise<boolean>;
+}
+
+export type TUserRole = keyof typeof USER_ROLE;
